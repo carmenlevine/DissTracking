8b0cbafe4f33db47926c811c7e6937dcf99dc4b4
carmenlevine <carmen.levine118@gmail.com>
2021-02-07 14:16:31 +0000

    Dissertation tracking app. App does not appear in the emulator. I have found the error to be between the lines 35 and 38 as this is where the systems stops compling after I have tested it.

---
+++ app/src/main/java/com/example/disstrackingapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.disstrackingapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    double lat;
+    double lon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String [] requiredPermissions = {
+                Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        boolean ok = true;
+        for (int i=0; i<requiredPermissions.length; i++){
+            int result = ActivityCompat.checkSelfPermission(this, requiredPermissions[i]);
+            if (result != PackageManager.PERMISSION_GRANTED){
+                ok = false;
+            }
+        }
+        if (!ok){ //error - doesn't compile from this point
+            ActivityCompat.requestPermissions(this,requiredPermissions,1);
+            System.exit(0);
+        } else { //getting GPS location through latitude and longitude
+            LocationManager locationManager = (LocationManager) getSystemService((LOCATION_SERVICE));
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    lat = location.getLatitude();
+                    lon = location.getLongitude();
+                }
+            });
+        }
+        TextView textLat = (TextView) findViewById(R.id.LatText);
+        textLat.setText("Latitude = " + lat);
+
+        TextView textLon = (TextView) findViewById(R.id.LongText);
+        textLon.setText("Longitude = " + lon);
+    }
+}
