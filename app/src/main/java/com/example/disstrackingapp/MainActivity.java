3148e94ff7cc2dba8ee770f81680f715f58b114d
carmenlevine <carmen.levine118@gmail.com>
2021-04-20 10:49:03 +0100

    Bug fixes to allow location to not appear as null.

--- app/src/main/java/com/example/disstrackingapp/MainActivity.java
+++ app/src/main/java/com/example/disstrackingapp/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.disstrackingapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.os.Bundle;
 import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationListener;
-
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
     double lat;
     double lon;
-    String provider;
     String latitude, longitude;
 
 
@@ -30,36 +25,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String [] requiredPermissions = {
-                Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+                Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION };
         boolean ok = true;
         for (int i=0; i<requiredPermissions.length; i++){
             int result = ActivityCompat.checkSelfPermission(this, requiredPermissions[i]);
             if (result != PackageManager.PERMISSION_GRANTED){
+                //Toast.makeText(this, "Permissions not granted", Toast.LENGTH_SHORT).show();
                 ok = false;
-            } else {
+            } else { //hits this
+                //Toast.makeText(this, "Permissions granted", Toast.LENGTH_SHORT).show();
                 ok = true;
             }
         }
-        if (ok){ //getting GPS location through latitude and longitude
-            LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (location != null){
-                lat = location.getLatitude();
-                lon = location.getLongitude();
-                latitude = String.valueOf(lat);
-                longitude = String.valueOf(lon);
+           if (ok){ //getting GPS location through latitude and longitude
+               //Toast.makeText(this, "Hits if ok function", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, requiredPermissions, 1);
+            LocationManager locationManager = (LocationManager)getSystemService((LOCATION_SERVICE));
+            //Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    lat = location.getLatitude();
+                    lon = location.getLongitude();
 
-                TextView textLat = (TextView) findViewById(R.id.LatText);
-                textLat.setText("Latitude = " + latitude);
+                    latitude = String.valueOf(lat);
+                    longitude = String.valueOf(lon);
 
-                TextView textLon = (TextView) findViewById(R.id.LongText);
-                textLon.setText("Longitude = " + longitude);
-            }
+                    TextView textLat = (TextView) findViewById(R.id.LatText);
+                    textLat.setText("Latitude = " + latitude);
 
-        } else { //error - doesn't compile from this point
+                    TextView textLon = (TextView) findViewById(R.id.LongText);
+                    textLon.setText("Longitude = " + longitude);
+                }
+            });
+
+        } else { //error
             Toast.makeText(this, "Unable to find location", Toast.LENGTH_SHORT).show();
-            //ActivityCompat.requestPermissions(this, requiredPermissions, 1);
+
             System.exit(0);
         }
+
+        }
     }
-}
