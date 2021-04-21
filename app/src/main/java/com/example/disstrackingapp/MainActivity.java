f2c382c769fd613665465616431c3dc8cb87706a
carmenlevine <carmen.levine118@gmail.com>
2021-04-21 16:33:06 +0100

    Deleted unused imports, commented out code and added comments to explain the code.

--- app/src/main/java/com/example/disstrackingapp/MainActivity.java
+++ app/src/main/java/com/example/disstrackingapp/MainActivity.java
@@ -15,16 +15,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -36,8 +26,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,28 +38,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Ensures the required permissions have been enabled by the device
         String[] requiredPermissions = {
                 Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
         boolean ok = true;
         for (int i = 0; i < requiredPermissions.length; i++) {
             int result = ActivityCompat.checkSelfPermission(this, requiredPermissions[i]);
             if (result != PackageManager.PERMISSION_GRANTED) {
-                //Toast.makeText(this, "Permissions not granted", Toast.LENGTH_SHORT).show();
-                ok = false;
-            } else { //hits this
-                //Toast.makeText(this, "Permissions granted", Toast.LENGTH_SHORT).show();
-                ok = true;
+                ok = false; //if permissions haven't been granted, assign ok as false
+            } else {
+                ok = true; // if permissions have been granted, assign ok as true
             }
         }
         if (ok) { //getting GPS location through latitude and longitude
-            //Toast.makeText(this, "Hits if ok function", Toast.LENGTH_SHORT).show();
             ActivityCompat.requestPermissions(this, requiredPermissions, 1);
             LocationManager locationManager = (LocationManager) getSystemService((LOCATION_SERVICE));
-            //Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
                 @Override
                 public void onLocationChanged(@NonNull Location location) {
+                    //When location is changed, extract the latitude and longitude and assign it to the values
+                    //of lat and lon then assign it to latitude and longitude values as strings so they can be
+                    //printed to the screen and passed to the database.
                     lat = location.getLatitude();
                     lon = location.getLongitude();
 
@@ -86,32 +73,36 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-        } else { //error
+        } else { //if the permissions haven't been granted
             Toast.makeText(this, "Unable to find location", Toast.LENGTH_SHORT).show();
-
             System.exit(0);
         }
 
+        //Identifies the submit button, so that when this is clicked, the latitude and longitude will be
+        //sent to the php file, which will then forward it onto the php my admin database as a new record.
         Button SubButton;
         SubButton = (Button) findViewById(R.id.submitButton);
         SubButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //php file containing connection to the database
                 String connstr = "http://192.168.1.79/location.php";
 
-
                 class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
-
                     @Override
                     protected String doInBackground(String... params) {
 
                         try {
+                            URL url = new URL(connstr); //converts the link from string to url
+                            HttpURLConnection http = (HttpURLConnection) url.openConnection(); //starts connection
 
-                            URL url = new URL(connstr);
-                            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                            //sets the nature of the request to post, so the lat and long can be posted into the database
                             http.setRequestMethod("POST");
                             http.setDoInput(true);
                             http.setDoOutput(true);
+
+                            //Setting up the output stream and buffered writer. Here the latitude and longitude is encoded using UTF-8
+                            //so that the data can be sent over the web securely.
                             OutputStream outputStream = http.getOutputStream();
                             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
                             String postData = URLEncoder.encode("latitude", "UTF-8") + "=" + URLEncoder.encode(latitude, "UTF-8") + "&"
@@ -120,6 +111,9 @@ public class MainActivity extends AppCompatActivity {
                             bufferedWriter.flush();
                             bufferedWriter.close();
                             outputStream.close();
+
+                            //Setting up the input stream and buffered reader. Here defines that each record will be separated by a line
+                            //so that it can be classified as two separate records in the database.
                             InputStream inputStream = http.getInputStream();
                             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
                             String result = "";
@@ -129,7 +123,8 @@ public class MainActivity extends AppCompatActivity {
                             }
                             bufferedReader.close();
                             inputStream.close();
-                            http.disconnect();
+                            http.disconnect(); //closes the connection
+
                             return result;
 
 
@@ -139,37 +134,13 @@ public class MainActivity extends AppCompatActivity {
                             e.printStackTrace();
                         }
 
-//                        String LatHolder = latitude;
-//                        String LonHolder = longitude;
-//
-//                        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-//
-//                        nameValuePairs.add(new BasicNameValuePair("latitude", LatHolder));
-//                        nameValuePairs.add(new BasicNameValuePair("longitude", LonHolder));
-//
-//                        try {
-//                            HttpClient httpClient = new DefaultHttpClient();
-//
-//                            HttpPost httpPost = new HttpPost(connstr);
-//
-//                            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-//
-//                            HttpResponse httpResponse = httpClient.execute(httpPost);
-//
-//                            HttpEntity httpEntity = httpResponse.getEntity();
-//
-//                        } catch (ClientProtocolException e) {
-//
-//                        } catch (IOException e) {
-//
-//                        }
-
                         return "Data Inserted Successfully";
                     }
 
                     @Override
                     protected void onPostExecute(String result) {
-                        super.onPostExecute(result);
+                        //Notifies the user that their data has been uploaded successfully so that multiple records aren't added
+                        super.onPostExecute(result); //result is passed from the do in background method
                         Toast.makeText(MainActivity.this, "Data Submitted Successfully", Toast.LENGTH_LONG).show();
                     }
                 }
@@ -178,18 +149,7 @@ public class MainActivity extends AppCompatActivity {
 
                 sendPostReqAsyncTask.execute(latitude, longitude);
 
-
                 }
             });
-
-
         }
     }
-
-
-
-
-
-
-
-
